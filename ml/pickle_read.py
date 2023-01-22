12a6426fb557dcede32a220678959455a43e8644
nitheesh1904 <nitheesh1904@gmail.com>
2023-01-22 13:03:25 +0530

    json

---
+++ ml/pickle_read.py
@@ -0,0 +1,8 @@
+import pickle
+import keras
+import json
+import tensorflow as tf
+from keras.models import model_from_json
+with open("ml\model.json","r") as file:
+    model_json=file.read()
+loaded_model=model_from_json(model_json)    
