0281c58c6f0a9700eb7a50d526af4452894178f5
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2023-01-22 13:58:43 +0530

    website is responsive!!!!!!!

--- ml/pickle_read.py
+++ ml/pickle_read.py
@@ -5,4 +5,5 @@ import tensorflow as tf
 from keras.models import model_from_json
 with open("ml\model.json","r") as file:
     model_json=file.read()
-loaded_model=model_from_json(model_json)    
+loaded_model=model_from_json(model_json)
+print(loaded_model.predict([1]*32)) 
