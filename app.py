e1f89c5d2e6ee7d4f2d4040550b8440df2a7f24b
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2023-01-23 19:35:11 +0530

    backend over

--- app.py
+++ app.py
@@ -11,9 +11,23 @@ def index():
 @app.route("/home", methods=['GET','POST'])
 def form():
     if request.method == 'POST':
+        job_type = request.form['job_type'] 
+        property = request.form['property'] 
+        other_loans = request.form['other_loans'] 
+        sab = request.form['sab'] 
+        cab = request.form['cab'] 
+        loan_history = request.form['loan_history'] 
+        g_co = request.form['g_co'] 
+        marital = request.form['marital'] 
         gender = request.form['gender'] 
-        flash(gender, 'success')
-        return redirect(url_for('index'))
+        maintanence = request.form['maintanence'] 
+        loan_amount = request.form['loan_amount'] 
+        no_loans = request.form['no_loans'] 
+        age = request.form['age'] 
+        income = request.form['income'] 
+        time = request.form['time'] 
+        flash([job_type, property, other_loans, sab, cab, loan_amount, loan_history, no_loans, other_loans, g_co, marital, gender, maintanence, age, income, time])
+        return redirect(url_for('form'))
     return render_template("form.html")
 
 if __name__ == "__main__":
